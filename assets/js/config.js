0095104e714cbc05d6d1cc4e77c4fd9abe6223e2
karam karam <116016222+Karamkaram17@users.noreply.github.com>
2026-03-20 15:29:47 +0200

    Update By Karam Karam

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,11 +1,14 @@
 window.PortColorsConfig = {
   ui: {
-    enableAddPort: true,
-    enableRemovePort: true,
-    enableCompare: true,
-    showCopy: true,
-    showReset: true,
-    showDownload: false,
-    showInfoSection: false,
-  },
+  "serviceId": "69bd4a4934e95ce81efe20e4",
+  "data": {
+    "enableAddPort": true,
+    "enableRemovePort": true,
+    "enableCompare": true,
+    "showCopy": true,
+    "showReset": true,
+    "showDownload": false,
+    "showInfoSection": false
+  }
+},
 };
